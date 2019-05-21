bea0451e726a3d5c43f2caee045d21bddec6fc89
Pouya Oftadeh <poftadeh2@gmail.com>
2019-05-20 22:25:00 -0400

    Add css loader and styles.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/app.js',
+  entry: "./src/app.js",
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
   },
   module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/,
-    }],
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: "cheap-module-eval-source-map",
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-  },
+    contentBase: path.join(__dirname, "public")
+  }
 };
