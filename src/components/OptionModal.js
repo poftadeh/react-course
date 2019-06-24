09092a3cd4e2aece2b0df70a84d083eb152b2d2d
Pouya Oftadeh <poftadeh2@gmail.com>
2019-06-24 03:26:01 -0400

    Finish css styling section

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Modal from 'react-modal';
 
-const OptionModal = (props) => (
+const OptionModal = props => (
   <Modal
     isOpen={!!props.selectedOption}
     contentLabel="selected option"
+    onRequestClose={props.handleClearSelectedOption}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected Option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>Okay</button>
+    <h3 className="modal__title">Selected Option</h3>
+    {props.selectedOption && (
+      <p className="modal__body">{props.selectedOption}</p>
+    )}
+    <button className="button" onClick={props.handleClearSelectedOption}>
+      Okay
+    </button>
   </Modal>
 );
 
