02daac0b7901b2750dd2ca98b8a07ab6ff8344d8
Pouya Oftadeh <poftadeh2@gmail.com>
2019-06-30 00:23:14 -0400

    Add navlinks

--- src/app.js
+++ src/app.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const Foo = () => <div>sfsdf</div>;
 
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>
+      Dashboard
+    </NavLink>
+    <NavLink to="/create" activeClassName="is-active">
+      Create Expense
+    </NavLink>
+    <NavLink to="/edit" activeClassName="is-active">
+      Edit Expense
+    </NavLink>
+    <NavLink to="/help" activeClassName="is-active">
+      Help
+    </NavLink>
+  </header>
+);
+
 const routes = (
   <BrowserRouter>
     <div>
-      <Route path="/" component={Foo} exact={true} />
-      <Route path="/create" component={Foo} />
-      <Route path="/edit" component={Foo} />
-      <Route path="/help" component={Foo} />
+      <Header />
+      <Switch>
+        <Route path="/" component={Foo} exact={true} />
+        <Route path="/create" component={Foo} />
+        <Route path="/edit" component={Foo} />
+        <Route path="/help" component={Foo} />
+        <Route component={Foo} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
