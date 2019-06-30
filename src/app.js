7a70cfc7279dcb2e93fafb128979fab8983b17d4
Pouya Oftadeh <poftadeh2@gmail.com>
2019-06-29 23:50:19 -0400

    Add react-router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>test</p>, document.getElementById('app'));
+const Foo = () => <div>sfsdf</div>;
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={Foo} exact={true} />
+      <Route path="/create" component={Foo} />
+      <Route path="/edit" component={Foo} />
+      <Route path="/help" component={Foo} />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
