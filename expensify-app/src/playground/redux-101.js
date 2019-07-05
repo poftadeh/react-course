fd6977c08956078262dd82ba7c7a035cf22fd324
Pouya Oftadeh <poftadeh2@gmail.com>
2019-07-05 02:28:17 -0400

    feat: add eslint, and commitlint

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -2,29 +2,42 @@ import { createStore } from 'redux';
 
 const store = createStore((state = { count: 0 }, action) => {
   console.log('running');
+  const incrementBy =
+    typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+  const decrementBy =
+    typeof action.decrementBy === 'number' ? action.decrementBy : 1;
   switch (action.type) {
     case 'INCREMENT':
       return {
-        count: state.count + 1,
+        count: state.count + incrementBy,
       };
     case 'DECREMENT':
       return {
-        count: state.count - 1,
+        count: state.count - decrementBy,
       };
     case 'RESET':
       return {
         count: 0,
+      };
+    case 'SET':
+      return {
+        count: action.count,
       };
     default:
       return state;
   }
 });
 
-console.log('TCL: store.getState()', store.getState());
+const unsubscribe = store.subscribe(() => {
+  console.log('TCL: store.getState()', store.getState());
+});
 
 store.dispatch({
   type: 'INCREMENT',
+  incrementBy: 5,
 });
+
+unsubscribe();
 
 store.dispatch({
   type: 'INCREMENT',
@@ -36,6 +49,10 @@ store.dispatch({
 
 store.dispatch({
   type: 'DECREMENT',
+  decrementBy: 10,
 });
 
-console.log('TCL: store.getState()', store.getState());
+store.dispatch({
+  type: 'SET',
+  count: 101,
+});
--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -4,24 +4,24 @@ module.exports = {
   entry: './src/playground/redux-101.js',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         loader: 'babel-loader',
         test: /\.js$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 };
