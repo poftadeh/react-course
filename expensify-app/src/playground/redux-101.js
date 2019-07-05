71d51bb2134ea101bb63e6219d6f61a4500083a0
Pouya Oftadeh <poftadeh2@gmail.com>
2019-07-05 01:11:32 -0400

    Dispatch redux actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,41 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  console.log('running');
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1,
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1,
+      };
+    case 'RESET':
+      return {
+        count: 0,
+      };
+    default:
+      return state;
+  }
+});
+
+console.log('TCL: store.getState()', store.getState());
+
+store.dispatch({
+  type: 'INCREMENT',
+});
+
+store.dispatch({
+  type: 'INCREMENT',
+});
+
+store.dispatch({
+  type: 'RESET',
+});
+
+store.dispatch({
+  type: 'DECREMENT',
 });
 
 console.log('TCL: store.getState()', store.getState());
