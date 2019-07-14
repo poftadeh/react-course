34feea50e5e1b06377efc33471ee16c7d03025a4
Pouya Oftadeh <poftadeh2@gmail.com>
2019-07-14 12:28:38 -0400

    feat: add expense reducers

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,19 +1,91 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
-const demoState = {
-  expenses: [
-    {
-      id: 'qweqwe',
-      description: 'January Rent',
-      note: 'This is a note',
-      amount: 54500,
-      createdAt: 0,
-    },
-  ],
-  filters: {
-    text: 'rent',
-    sortBy: 'amount',
-    startDate: undefined,
-    endDate: undefined,
+const addExpense = ({
+  description = '',
+  note = '',
+  amount = 0,
+  createdAt = 0,
+} = {}) => ({
+  type: 'ADD_EXPENSE',
+  expense: {
+    id: uuid(),
+    description,
+    note,
+    amount,
+    createdAt,
   },
+});
+
+const removeExpense = ({ id } = {}) => ({
+  type: 'REMOVE_EXPENSE',
+  id,
+});
+
+const expensesReducerDefaultState = [];
+
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined,
 };
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      console.log('action', action);
+      return state.filter(({ id }) => id !== action.id);
+    default:
+      return state;
+  }
+};
+
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer,
+  }),
+);
+
+store.subscribe(() => {
+  console.log('subscribe', store.getState());
+});
+
+const expenseOne = store.dispatch(
+  addExpense({ description: 'Rent', amount: 100 }),
+);
+// const expenseTwo = store.dispatch(
+//   addExpense({ description: 'Coffee', amount: 500 }),
+// );
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+console.log('getState()', store.getState());
+
+// const demoState = {
+//   expenses: [
+//     {
+//       id: 'qweqwe',
+//       description: 'January Rent',
+//       note: 'This is a note',
+//       amount: 54500,
+//       createdAt: 0,
+//     },
+//   ],
+//   filters: {
+//     text: 'rent',
+//     sortBy: 'amount',
+//     startDate: undefined,
+//     endDate: undefined,
+//   },
+// };
