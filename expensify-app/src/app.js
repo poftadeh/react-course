4497d72ec033cb0601f3ce0b8c4568522c1e96ea
Pouya Oftadeh <poftadeh2@gmail.com>
2019-07-17 05:39:12 -0400

    refactor: redux code

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import getVisibleExpenses from './selectors/expenses';
+import { setTextFilter } from './actions/filter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.subscribe(() => {
+  const state = store.getState();
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+  console.log(visibleExpenses);
+});
+store.dispatch(addExpense({ description: 'water bill' }));
+store.dispatch(addExpense({ description: 'gas bill' }));
+store.dispatch(setTextFilter('gas'));
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ expensify-app/src/reducers/expenses.js
@@ -0,0 +1,23 @@
+const expensesReducerDefaultState = [];
+
+export default (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    case 'ADD_EXPENSE':
+      return [...state, action.expense];
+    case 'REMOVE_EXPENSE':
+      console.log('action', action);
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map(expense => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates,
+          };
+        }
+        return expense;
+      });
+    default:
+      return state;
+  }
+};
